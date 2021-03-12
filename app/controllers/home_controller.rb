483e166de811ac8c63e63b316ed74fb9581c7342
GT301 <taoguo301@gmail.com>
2021-03-12 14:59:56 -0600

    Modified Home controller and Home index view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
+    @clubs = Club.includes(:league)
+                 .order("club_name DESC")
+                 .limit(20)
+    @leagues = League.limit(10)
   end
 end

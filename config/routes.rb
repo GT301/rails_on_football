ff58fe482f02e7837a99a80e75b1cc239775c26b
GT301 <taoguo301@gmail.com>
2021-03-12 11:05:57 -0600

    Added Clubs, Leagues and Matches controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'matches/index'
+  get 'matches/show'
+  get 'leagues/index'
+  get 'leagues/show'
+  get 'clubs/index'
+  get 'clubs/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
